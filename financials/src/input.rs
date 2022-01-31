0a135af7ca30f44e126bc86729465ffc96cb34ca
Ashwin Chatterji <ashwinath@hotmail.com>
2022-01-31 04:31:54 +0000

    Change to return result instead of unwrap.

--- financials/src/input.rs
+++ financials/src/input.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Utc};
 use serde::Deserialize;
 
+use std::error::Error;
+
 #[derive(Debug, Deserialize)]
 pub struct Trade {
     #[serde(with = "yymmdd_format")]
@@ -29,31 +31,42 @@ mod yymmdd_format {
     }
 }
 
-pub fn read_from_csv<T>(csv: &str) -> Vec<T>
+pub fn read_from_csv<T>(csv: &str) -> Result<Vec<T>, Box<dyn Error>>
     where
     T: for<'de> serde::Deserialize<'de>,
 {
 
-    let mut rdr = csv::Reader::from_path(csv).unwrap();
+    let mut rdr = csv::Reader::from_path(csv)?;
 
     let mut data = Vec::new();
 
     for result in rdr.deserialize() {
-        let record: T = result.unwrap();
+        let record: T = result?;
         data.push(record);
     }
 
-    data
+    Ok(data)
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::{Utc, TimeZone};
 
     #[test]
     fn parse_csv() {
-        let result: Vec<Trade> = read_from_csv("./test/trade.csv");
-        println!("{:?}", result);
+        let result: Vec<Trade> = read_from_csv("./test/trade.csv").unwrap();
+        assert_eq!(result.len(), 1);
+
+        let result = &result[0];
+        assert_eq!(result.symbol, "IWDA.LON");
+        assert_eq!(result.trade_type, "buy");
+        assert_eq!(result.price_each, 76.34);
+        assert_eq!(result.quantity, 10.0);
+        let expected_date: DateTime<Utc> = Utc
+            .datetime_from_str("2021-03-11 16:00:00", "%Y-%m-%d %H:%M:%S")
+            .unwrap();
+        assert_eq!(result.date_purchased, expected_date);
     }
 }
--- financials/src/main.rs
+++ financials/src/main.rs
@@ -7,6 +7,6 @@ mod input;
 fn main() {
     let c = Config::new();
 
-    let result: Vec<Trade> = read_from_csv(&c.stocks_csv);
+    let result: Vec<Trade> = read_from_csv(&c.stocks_csv).unwrap();
     println!("{:?}", result);
 }
